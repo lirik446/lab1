3569c62d5756eeab47050d863d4928037c8bdfd1
lirik446 <lirikrlej@gmail.com>
2025-09-15 09:30:23 +0300

    Update p9.cpp

--- p9.cpp
+++ p9.cpp
@@ -2,28 +2,35 @@
 
 int main()
 {
-	int h = 0, min = 0, h1 = 0, min1 = 0, time = 0, timeh = 0, timemin = 0;
-	//double time = 0;
+	int h = 0, min = 0, hDepart = 0, minDepart = 0, arrTime = 0, depTime = 0, timeH = 0, timeMin = 0;
 	std::cout << "Enter the hour and minute of arrival\n" << "h: ";
 	std::cin >> h;
 	std::cout << "min: ";
 	std::cin >> min;
 	std::cout << "Enter the hour and minute of departure\n" << "h: ";
-	std::cin >> h1;
+	std::cin >> hDepart;
 	std::cout << "min: ";
-	std::cin >> min1;
-	h = h * 60;
-	h1 = h1 * 60;
-	time = ((h1 + min1) - (h + min));
-	if (time > 59)
+	std::cin >> minDepart;
+	if (h > 23 || h < 0 || min < 0 || min > 59 || hDepart > 23 || hDepart < 0 || minDepart < 0 || minDepart > 59)
 	{
-		timeh = time / 60;
-		timemin = time - timeh * 60;
-		std::cout << "Parking time " << timeh << ":" << timemin;
+		std::cout << "incorrect time format";
+		return 0;
+	}
+	h = h * 60;
+	hDepart = hDepart * 60;
+	arrTime = h + min;
+	depTime = hDepart + minDepart;
+	if (arrTime > depTime)
+	{
+		timeH = (1440 - arrTime + depTime) / 60;
+		timeMin = (1440 - arrTime + depTime) - timeH * 60;
+		std::cout << "Parking time " << timeH << ":" << timeMin;
 	}
 	else
 	{
-		std::cout << "Parking time 00:" << time;
+		timeH = (depTime - arrTime) / 60;
+		timeMin = (depTime - arrTime) - timeH * 60;
+		std::cout << "Parking time " << timeH << ":" << timeMin;
 	}
 	return 0;
 }
